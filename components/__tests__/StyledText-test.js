62909f352122e3d7e45c66f9548566613bb2ceb2
degerahmet <adeger34@gmail.com>
2025-05-28 16:04:39 -0700

    StyledText.test file updated

--- components/__tests__/StyledText-test.js
+++ components/__tests__/StyledText-test.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import renderer, { act } from 'react-test-renderer'; // Import act from react-test-renderer
 import { it, expect } from '@jest/globals';
 
 import { MonoText } from '../StyledText';
 
 it(`renders correctly`, () => {
-  const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
+  let tree;
+  act(() => {
+    tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
+  });
 
   expect(tree).toMatchSnapshot();
 });
